1cc2280ac389c7d1cce532165577606044d09acc
guimileib <guilhermemileib@gmail.com>
2025-03-24 09:06:08 -0300

    improve: Adding validator in registry_order

--- src/use_cases/registry_order.py
+++ src/use_cases/registry_order.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from src.models.repository.interfaces.orders_repository import OrdersRepositoryInterface
 from src.main.http_types.http_request import HttpRequest
 from src.main.http_types.http_response import HttpResponse
+from src.validators.registry_order_validator import registry_order_validator
 
 class RegistryOrder:
     def __init__(self, orders_repository: OrdersRepositoryInterface) -> None:
@@ -11,15 +12,20 @@ class RegistryOrder:
     def registry(self, http_request: HttpRequest ) -> HttpResponse:
         try:
             body = http_request.body
-            new_order = self.__format_new_order(body)
-            self.__registry_order(new_order)
+            self.__validate_body(body) # Extração e validação do conteúdo
             
-            return self.__format_reponse()
+            new_order = self.__format_new_order(body) # Formatação dos dados
+            self.__registry_order(new_order) # Registro do novo dado/pedido
+                        
+            return self.__format_reponse() # Formatação de resposta do dado
         except Exception as exception: # Fazer um teste unitario para esse erro
             return HttpResponse(
-                body= { "error": exception }, 
+                body= { "error": str(exception) }, 
                 status_code=400
             )
+            
+    def __validate_body(self, body: dict) -> None:
+        registry_order_validator(body) # Tratamento com o body 
     
     # a logica tem uma operação e o formato da reposta outra
     def __format_new_order(self, body: dict) -> dict:
